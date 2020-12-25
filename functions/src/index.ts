e0f19d350e1a8b8edd8fafb8a4d6a86da0b4680a
anirbmuk <anirbmuk@live.in>
2020-12-25 10:49:26 +0530

    Updated message from cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,6 @@ const cors = require('cors')({origin: true});
 
 export const autocicd = functions.https.onRequest((request, response) => {
  cors(request, response, () => {
-  response.status(200).send({ message: 'Response from autocicd function' });
+  response.status(200).send({ message: `Current time is ${new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"})}.` });
  });
 });
